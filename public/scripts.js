fc2f11e1ec0fcef6507822006fb0080b71028bff
esklotz <joana.estefani@gmail.com>
2020-07-28 10:01:23 +0200

    Projeto Launchstore gerado no modulo 06 do curso da Rocketseat

--- public/scripts.js
+++ public/scripts.js
@@ -13,3 +13,50 @@ const Mask = {
         }).format(value/100)
     }
 }
+
+const PhotosUpload = {
+    preview: document.querySelector('#photos-preview').appendChild(div),
+    uploadLimit: 6,
+    handleFileInput(event) {
+        const {files : fileList} = event.target
+
+        if(PhotosUpload.hasLimit(event)) return
+
+        Array.from(fileList).forEach(file => {
+            const reader = new FileReader()
+
+            reader.onload = () => {
+                const image = new Image()
+                image.src = String(reader.result)
+
+                const div = PhotosUpload.getContainer(image)
+                PhotosUpload.preview.appendChild(div)
+            }
+
+            reader.readAsDataURL(file)
+        })
+    },
+    hasLimit(event) {
+        const {uploadLimit} = PhotosUpload
+
+        if (fileList.length > uploadLimit) {
+            alert(`Envie no máximo ${uploadLimit} fotos!`)
+            event.preventDefault()
+            return true
+        }
+
+        return false
+    },
+    getContainer(image) {
+        const div = document.createElement('div')
+                div.classList.add('photo')
+
+                div.onclick = () => alert('Remover foto')
+
+                div.appendChild(image)
+
+                return div
+    }
+
+
+}
