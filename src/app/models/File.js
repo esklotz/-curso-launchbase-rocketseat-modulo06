904d759bd92947a433eb45e72d9cfa816be74540
esklotz <joana.estefani@gmail.com>
2020-08-06 19:03:35 +0200

    Projeto Launchstore gerado no curso Launchbase da Rocketseat

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -2,7 +2,7 @@ const db = require('../../config/db')
 const fs = require('fs')
 
 module.exports = {
-    create(filename, path, product_id) {
+    create({filename, path, product_id}) {
         const query = `
     INSERT INTO files (
         name,
